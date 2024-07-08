23c921a352d276b6b74e71eb97a72377f2e3f75f
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-07-07 23:50:28 -0300

    Create node.hpp

---
+++ node.hpp
@@ -0,0 +1,50 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <vector>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <functional>
+
+
+struct Node : std::enable_shared_from_this<Node> {
+	std::vector<std::shared_ptr<Node>> parents;
+	std::string name;
+
+	Node(const std::string& n = "") : name(n) {
+
+	}
+
+    virtual inline void evaluate() = 0;
+    virtual inline void derive() = 0;
+
+
+    std::vector<std::shared_ptr<Node>> topologicalSort() {
+    	std::vector<std::shared_ptr<Node>> ordering;
+		std::unordered_set<std::shared_ptr<Node>> visited;
+
+		std::function<void(const std::shared_ptr<Node>&)> addChildren = [&](const std::shared_ptr<Node>& node) {
+			// if the node has no parents, we shouldn't need to either evaluate nor derive it
+			if (visited.find(node) != visited.end() || !node->parents.size()) {
+				return;
+			}
+
+			visited.insert(node);
+			for (size_t i = 0; i < node->parents.size(); ++i) {
+				addChildren(node->parents[i]);
+			}
+
+			ordering.push_back(node);
+		};
+
+		// this does require this Node to have a shared_ptr to it, 
+		// but there's no situation where it shouldn't have one, so it's fine
+		addChildren(shared_from_this());
+
+		return ordering;
+	}
+};
+
+
+#endif

e5b86afb98663cc1c65b80a405a89fa67d5c0a51
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-07-09 00:06:21 -0300

    Update scalar.hpp

--- scalar.hpp
+++ scalar.hpp
@@ -3,40 +3,47 @@
 
 #include "node.hpp"
 
+#include <iostream>
+
 
 struct Scalar : Node {
 
-    float value;
-    float partial;
+	float value;
+	float partial;
 
-    Scalar(float v = 0.0f, const std::string& n = "") : value(v), partial(0.0f) {
-        name = (n == "") ? std::to_string(v) : n;
-    }
+	Scalar(float v = 0.0f, const std::string& n = "") : value(v), partial(0.0f) {
+		name = (n == "") ? std::to_string(v) : n;
+	}
 
-    static std::shared_ptr<Scalar> build(float v = 0.0f, const std::string& n = "") {
-        return std::make_shared<Scalar>(v, n);
-    }
+	static std::shared_ptr<Scalar> build(float v = 0.0f, const std::string& n = "") {
+		return std::make_shared<Scalar>(v, n);
+	}
 
-    void evaluate() override {
+	void evaluate() override {
 
-    }
+	}
 
-    void derive() override {
+	void derive() override {
 
-    }
+	}
 
-    void calculateDerivatives() {
-    	std::vector<std::shared_ptr<Node>> ordering = topologicalSort();
+	void resetPartial() override {
+		partial = 0.0f;
+	}
 
-	    for (size_t i = 0; i < ordering.size(); ++i) {
-	        ordering[i]->evaluate();
+	void calculateDerivatives() {
+		std::vector<std::shared_ptr<Node>> ordering = topologicalSort();
+
+		for (size_t i = 0; i < ordering.size(); ++i) {
+			ordering[i]->evaluate();
+			ordering[i]->resetPartial();
 		}
 
-	    partial = 1.0; // dx/dx is 1 for whatever x
-	    for (size_t i = ordering.size(); i > 0; --i) {
-	        ordering[i - 1]->derive();
+		partial = 1.0; // dx/dx is 1 for whatever x
+		for (size_t i = ordering.size(); i > 0; --i) {
+			ordering[i - 1]->derive();
 		}
-    }
+	}
 };
 
 // nicer naming

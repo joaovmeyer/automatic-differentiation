8591010f00e4e7fdce9d979e36e0c7f2bb799909
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-08-10 23:06:11 -0300

    Update vector.hpp

--- vector.hpp
+++ vector.hpp
@@ -12,6 +12,15 @@ inline void operator += (std::vector<float>& v1, const std::vector<float>& v2) {
 	}
 }
 
+inline std::vector<float> operator + (const std::vector<float>& v1, const std::vector<float>& v2) {
+	std::vector<float> ans = v1;
+	for (size_t i = 0; i < v1.size(); ++i) {
+		ans[i] += v2[i];
+	}
+
+	return ans;
+}
+
 inline std::vector<float> operator * (const std::vector<float>& v, float a) {
 	std::vector<float> ans = v;
 	for (size_t i = 0; i < ans.size(); ++i) {
@@ -45,7 +54,7 @@ struct Vector : Node {
 	std::vector<float> value;
 	std::vector<float> partial;
 
-	Vector(size_t s = 0, float fillValue = 0.0f, const std::string& n = "") : size(s), value(std::vector<float>(s, fillValue)), partial(std::vector<float>(s, 0.0f)) {
+	Vector(size_t s = 0, float fillValue = 0.0f, const std::string& n = "", bool trainable = false) : size(s), value(std::vector<float>(s, fillValue)), partial(std::vector<float>(s, 0.0f)) {
 		
 		#if USE_NAME
 			if (n == "") {
@@ -62,10 +71,12 @@ struct Vector : Node {
 				name = n;
 			}
 		#endif
+
+		isTrainable = trainable;
 	}
 
-	static std::shared_ptr<Vector> build(size_t s = 0, float fillValue = 0.0f, const std::string& n = "") {
-		return std::make_shared<Vector>(s, fillValue, n);
+	static std::shared_ptr<Vector> build(size_t s = 0, float fillValue = 0.0f, bool trainable = false, const std::string& n = "") {
+		return std::make_shared<Vector>(s, fillValue, n, trainable);
 	}
 
 	void evaluate() override {
@@ -76,8 +87,12 @@ struct Vector : Node {
 
 	}
 
-	void resetPartial() override {
+	void resetPartial() override final {
 		std::fill(partial.begin(), partial.end(), 0.0f);
+	}
+
+	NodeTypes getType() {
+		return VECTOR;
 	}
 
 
@@ -94,10 +109,100 @@ struct Vector : Node {
 			ordering[i - 1]->derive();
 		}
 	}
+
+	Var get(size_t index);
 };
 
 // nicer naming
 using Vec = std::shared_ptr<Vector>;
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct GetVectorElem : Scalar {
+	Vec a;
+	size_t index;
+
+	GetVectorElem() {}
+
+	static Var build(const Vec& v, size_t index = 0) {
+
+		std::shared_ptr<GetVectorElem> node = std::make_shared<GetVectorElem>();
+
+		node->a = v;
+		node->index = index;
+		#if USE_NAME
+			node->name = v->name + "[" + std::to_string(index) + "]";
+		#endif
+		node->value = v->value[index];
+
+		node->parents.push_back(v);
+
+		return node;
+	}
+
+	void evaluate() override final {
+		value = a->value[index];
+	}
+
+	void derive() override final {
+		a->partial[index] += partial;
+	}
+};
+
+
+Var Vector::get(size_t index = 0) {
+	return GetVectorElem::build(dynamic_pointer_cast<Vector>(shared_from_this()), index);
+}
+
+
+struct VectorFromScalars : Vector {
+	std::vector<Var> a;
+
+	VectorFromScalars(size_t s = 0, float fillValue = 0.0f) {
+		size = s;
+		value = std::vector<float>(s, fillValue);
+		partial = std::vector<float>(s, 0.0f);
+	}
+
+	static Vec build(const std::vector<Var>& vars) {
+
+		std::shared_ptr<VectorFromScalars> node = std::make_shared<VectorFromScalars>(vars.size());
+
+		node->a = vars;
+		for (size_t i = 0; i < vars.size(); ++i) {
+			node->parents.push_back(vars[i]);
+			node->value[i] = vars[i]->value;
+		}
+
+		return node;
+	}
+
+	void evaluate() override final {
+		for (size_t i = 0; i < size; ++i) {
+			value[i] = a[i]->value;
+		}
+	}
+
+	void derive() override final {
+		for (size_t i = 0; i < size; ++i) {
+			a[i]->partial += partial[i];
+		}
+	}
+};
+
+
+
+
 
 
 #endif

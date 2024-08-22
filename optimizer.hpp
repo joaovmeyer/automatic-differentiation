8d641f20ec339e53ea5915ff8708de3bbf6afb7a
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-08-21 23:42:16 -0300

    finally a better way to do it (I think)

--- optimizer.hpp
+++ optimizer.hpp
@@ -1,34 +1,66 @@
 #ifndef OPTIMIZER_HPP
 #define OPTIMIZER_HPP
 
-#include "node.hpp"
+#include "scalar.hpp"
+#include "vector.hpp"
+#include "matrix.hpp"
 #include <iostream>
+#include <variant>
 
 
-// this feels like really stupid implementation but who cares, right?
 
+using ScalarType = float;
+using VectorType = std::vector<float>;
+using MatrixType = std::vector<std::vector<float>>;
+
+ScalarType zerosLike(const ScalarType& a) {
+	return 0.0f;
+}
+
+VectorType zerosLike(const VectorType& a) {
+	return VectorType(a.size(), 0.0f);
+}
+
+MatrixType zerosLike(const MatrixType& a) {
+	return MatrixType(a.size(), VectorType(a[0].size(), 0.0f));
+}
+
+
+template <template<typename> class OptimizerParam>
 struct Optimizer {
 
 	std::vector<std::shared_ptr<Scalar>> scalarParameters;
 	std::vector<std::shared_ptr<Vector>> vectorParameters;
 	std::vector<std::shared_ptr<Matrix>> matrixParameters;
 
-	Optimizer(const std::shared_ptr<Node>& f) {
+    std::vector<OptimizerParam<ScalarType>> optimScalar;
+    std::vector<OptimizerParam<VectorType>> optimVector;
+    std::vector<OptimizerParam<MatrixType>> optimMatrix;
 
+	template<class... Types>
+	Optimizer(const std::shared_ptr<Node>& f, Types... args) {
+
+		// doing this to get every parameter that affects the function f
 		std::vector<std::shared_ptr<Node>> ordering = f->topologicalSort();
 
 		for (size_t i = 0; i < ordering.size(); ++i) {
+
+			// do not want to optimize non-trainable nodes
 			if (ordering[i]->isTrainable) {
 
+				// separate them in scalar, vector and matrix
 				switch (ordering[i]->getType()) {
 					case Node::SCALAR:
 						scalarParameters.push_back(std::dynamic_pointer_cast<Scalar>(ordering[i]));
+						optimScalar.push_back(OptimizerParam<ScalarType>(scalarParameters.back()->value, args...));
 						break;
 					case Node::VECTOR:
 						vectorParameters.push_back(std::dynamic_pointer_cast<Vector>(ordering[i]));
+						optimVector.push_back(OptimizerParam<VectorType>(vectorParameters.back()->value, args...));
 						break;
 					case Node::MATRIX:
 						matrixParameters.push_back(std::dynamic_pointer_cast<Matrix>(ordering[i]));
+						optimMatrix.push_back(OptimizerParam<MatrixType>(matrixParameters.back()->value, args...));
 						break;
 				}
 
@@ -36,57 +68,35 @@ struct Optimizer {
 		}
 	}
 
-	virtual void step() = 0;
-
+	void step() {
+		for (size_t i = 0; i < optimScalar.size(); ++i) {
+            optimScalar[i].step(scalarParameters[i]->value, scalarParameters[i]->partial);
+        }
+        for (size_t i = 0; i < optimVector.size(); ++i) {
+            optimVector[i].step(vectorParameters[i]->value, vectorParameters[i]->partial);
+        }
+        for (size_t i = 0; i < optimMatrix.size(); ++i) {
+            optimMatrix[i].step(matrixParameters[i]->value, matrixParameters[i]->partial);
+        }
+	}
 };
 
-using ScalarType = float;
-using VectorType = std::vector<float>;
-using MatrixType = std::vector<std::vector<float>>;
 
 
-struct SGDMomentum : Optimizer {
-
+template <typename T>
+struct Momentum {
 	float a, b;
+	T v;
 
-	std::vector<ScalarType> scalarV;
-	std::vector<VectorType> vectorV;
-	std::vector<MatrixType> matrixV;
-
-	SGDMomentum(const std::shared_ptr<Node>& f, float b = 0.9f, float a = 0.1f) : Optimizer(f), a(a), b(b) {
-		for (size_t i = 0; i < scalarParameters.size(); ++i) {
-			scalarV.push_back(0.0f);
-		}
-
-		for (size_t i = 0; i < vectorParameters.size(); ++i) {
-			vectorV.push_back(VectorType(vectorParameters[i]->size, 0.0f));
-		}
-
-		for (size_t i = 0; i < matrixParameters.size(); ++i) {
-			matrixV.push_back(MatrixType(matrixParameters[i]->rows, VectorType(matrixParameters[i]->cols, 0.0f)));
-		}
+	Momentum(const T& param, float a = 0.1f, float b = 0.9f) : a(a), b(b), v(zerosLike(param)) {
+		
 	}
 
-	void step() override final {
-		for (size_t i = 0; i < scalarParameters.size(); ++i) {
-			scalarV[i] = scalarV[i] * b + scalarParameters[i]->partial;
-			scalarParameters[i]->value += scalarV[i] * -a;
-		}
-
-		for (size_t i = 0; i < vectorParameters.size(); ++i) {
-			vectorV[i] = vectorV[i] * b + vectorParameters[i]->partial;
-			vectorParameters[i]->value += vectorV[i] * -a;
-		}
-
-		for (size_t i = 0; i < matrixParameters.size(); ++i) {
-			matrixV[i] = matrixV[i] * b + matrixParameters[i]->partial;
-			matrixParameters[i]->value += matrixV[i] * -a;
-		}
+	void step(T& param, const T& partial) {
+		v = v * b + partial;
+		param += v * -a;
 	}
-
 };
-
-
 
 
 

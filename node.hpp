c239ad1f43be805ed0420d318d1a90a5962b5416
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-09-08 19:13:39 -0300

    Update node.hpp

--- node.hpp
+++ node.hpp
@@ -6,17 +6,22 @@
 #include <string>
 #include <unordered_set>
 #include <functional>
+#include <tuple>
+#include <omp.h>
 
 
 // using the function's name can be helpfull for debugging, but sometimes it can break everything
 // in the GRU implementation, the functions names were getting REALLY big, bigger than my RAM,
 // so in this case it's best to avoid using the name at all
+#ifndef USE_NAME
 #define USE_NAME false
+#endif
 
 
 struct Node : std::enable_shared_from_this<Node> {
 	std::vector<std::shared_ptr<Node>> parents;
 	bool isTrainable;
+	bool isSlowOperation = false;
 
 	#if USE_NAME
 		std::string name;
@@ -33,7 +38,7 @@ struct Node : std::enable_shared_from_this<Node> {
 
 	virtual inline void evaluate() = 0;
 	virtual inline void derive() = 0;
-	virtual inline void resetPartial() = 0;
+	virtual inline void resetPartial(float defaultValue = 0.0f) = 0;
 	virtual inline NodeTypes getType() = 0;
 
 
@@ -62,15 +67,40 @@ struct Node : std::enable_shared_from_this<Node> {
 		return ordering;
 	}
 
+	void calculateDerivatives() {
+		std::vector<std::shared_ptr<Node>> ordering = topologicalSort();
+
+		for (size_t i = 0; i < ordering.size(); ++i) {
+			ordering[i]->evaluate();
+			ordering[i]->resetPartial();
+		}
+
+		// dx/dx is 1 for whatever x
+		resetPartial(1.0f);
+		for (size_t i = ordering.size(); i > 0; --i) {
+			ordering[i - 1]->derive();
+		}
+	}
+
+
+	using NodeMat = std::vector<std::vector<std::shared_ptr<Node>>>;
+	
+
 	// similar to topologicalSort, but separated in layers, where each layer can be processed in parallel
-	std::vector<std::vector<std::shared_ptr<Node>>> layeredTopologicalSort() {
-		std::vector<std::vector<std::shared_ptr<Node>>> layers;
+	// this is a very crude implementation, and probably I should do some tweaks to maximize the size
+	// of each slow layer, at least it makes sense to me that packing them together will be faster
+
+	// I also tried to separate them in slow layers and fast layers, because parallelizing the fast operations
+	// is not worth it. Probably there's a better way to do this, but that's for the future.
+	std::tuple<NodeMat, NodeMat> layeredTopologicalSort(bool excludeFirstGeneration = false) {
+		NodeMat layersSlow;
+		NodeMat layersFast;
 		std::unordered_set<std::shared_ptr<Node>> visited;
-        std::unordered_map<std::shared_ptr<Node>, int> layerNum;
+		std::unordered_map<std::shared_ptr<Node>, int> layerNum;
 
 		std::function<void(const std::shared_ptr<Node>&)> addChildren = [&](const std::shared_ptr<Node>& node) {
 			// already handled this node
-			if (visited.find(node) != visited.end()) {
+			if (visited.find(node) != visited.end() || (excludeFirstGeneration && !node->parents.size())) {
 				return;
 			}
 
@@ -85,16 +115,64 @@ struct Node : std::enable_shared_from_this<Node> {
 			int nodeLayer = maxParentLayer + 1;
 			layerNum[node] = nodeLayer;
 
-			if (nodeLayer >= layers.size()) layers.resize(nodeLayer + 1);
+			// resize both layers cause they should have the same size
+			layersSlow.resize(std::max(layersSlow.size(), (size_t) nodeLayer + 1));
+			layersFast.resize(std::max(layersFast.size(), (size_t) nodeLayer + 1));
 
-			layers[nodeLayer].push_back(node);
+			if (node->isSlowOperation) {
+				layersSlow[nodeLayer].push_back(node);
+			} else {
+				layersFast[nodeLayer].push_back(node);
+			}
 		};
 
 		addChildren(shared_from_this());
 
-		return layers;
+		return { layersFast, layersSlow };
 	}
 
+	// now this is generally not worth it, but if a given function has some heavy calculations that can be done in parallel,
+	// there's a chance the layered topological sort will arrange them together, and you might get some pretty good speed up.
+	// As an example, in an LSTM cell, there are 8 matrix by vector multiplications, and it just happens that it's parallel
+	// version will separate them in two groups of 4, and I tested it with a big LSTM layer (512 neurons) and I got a ~1.5x
+	// speed up over the non parallel version (execution time went from ~67 seconds to ~45 secunds)
+	void calculateDerivativesParallel() {
+
+		auto [layersFast, layersSlow] = layeredTopologicalSort();
+
+		for (size_t i = 0; i < layersSlow.size(); ++i) {
+
+			// for slow layers, try to parallelize what we can
+			#pragma omp parallel for schedule(dynamic) if(layersSlow[i].size() > 1)
+			for (size_t j = 0; j < layersSlow[i].size(); ++j) {
+				layersSlow[i][j]->evaluate();
+				layersSlow[i][j]->resetPartial();
+			}
+
+
+			// for fast layers, the overhead of parallelizing would actually cause a performance decrease
+			for (size_t j = 0; j < layersFast[i].size(); ++j) {
+				layersFast[i][j]->evaluate();
+				layersFast[i][j]->resetPartial();
+			}
+		}
+
+		// dx/dx is 1 for whatever x
+		resetPartial(1.0f);
+
+		for (size_t i = layersSlow.size(); i > 0; --i) {
+
+			#pragma omp parallel for schedule(dynamic) if(layersSlow[i - 1].size() > 1)
+			for (size_t j = 0; j < layersSlow[i - 1].size(); ++j) {
+				layersSlow[i - 1][j]->derive();
+			}
+
+
+			for (size_t j = 0; j < layersFast[i - 1].size(); ++j) {
+				layersFast[i - 1][j]->derive();
+			}
+		}
+	}
 
 
 	void eval() {
